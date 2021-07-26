61e3cd9dc87b2aea337b64d731c977ff0d7ea060
François PINOT <fggpinot@gmail.com>
2021-07-26 12:10:40 +0200

    Csound function notatation

--- examples/axa1.go
+++ examples/axa1.go
@@ -10,24 +10,24 @@ var orc string = `
 sr     = 44100
 ksmps  = 10
 nchnls = 2
+0dbfs  = 1.0
 
-          instr 1
+  instr 1
 ;p4 grain pointer (in seconds)
 ;p5 pan (0...1)
 
+ipanl = table(1-p5 ,4, 1)
+ipanr = table(p5 , 4, 1)
 
-ipanl	table	1-p5 ,4,1
-ipanr	table	p5 ,4,1
+andx  = line:a(p4, p3, p4+p3)
+asig  = tablei:a(andx*sr, 1)
 
-andx	line	p4,p3,p4+p3
-asig	tablei	andx*sr,1
-
-k1	oscil	30000,1/p3,2	
-asig	=	asig*k1
-	
-	outs	asig*ipanl, asig*ipanr
-	
-          endin`
+k1    = oscil(0.91, 1/p3, 2)
+asig  = asig*k1
+  
+        outs asig*ipanl, asig*ipanr
+  endin
+`
 
 var sco string = `
 f1 0 65536 1 "../samples/axaxaxas.aif" 0 4 1
@@ -50,11 +50,13 @@ func events() string {
 	p.Num, p.Gen = 3, gmasklib.ConstGen(0.04)
 	f.AddParam(p)
 
-	g := gmasklib.BpfGen([]float64{0, 1.44}, gmasklib.NewInterpolation(0, false, false))
+	g := gmasklib.BpfGen([]float64{0, 1.44}, gmasklib.NewInterpolation(0, false, true))
 	p.Num, p.Gen = 4, g
 	f.AddParam(p)
 
-	p.Num, p.Gen = 5, gmasklib.ConstGen(0.5)
+	//p.Num, p.Gen = 5, gmasklib.ConstGen(0.5)
+	p.Num, p.Gen = 5, gmasklib.BpfGen([]float64{0, 0, 2.5, 1, 5, 0}, 
+		gmasklib.NewInterpolation(0, false, true))
 	f.AddParam(p)
 
 	var buf bytes.Buffer
